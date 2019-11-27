ea02456d9f75b7400780172ac3e67665791d2ea8
Dana Ryashy <danriachi@hotmail.com>
2019-11-27 17:21:18 -0500

    added player controls

--- multiplayer_test/app.js
+++ multiplayer_test/app.js
@@ -10,12 +10,41 @@ app.use('/client', express.static(__dirname+'/client'));//else extract file
 
 serv.listen(2000);//using port 2000
 console.log('server started');
+//************************************************************//
+
+//create an array of sockets (clients) & players
+let SOCKET_LIST = {};
+let PLAYER_LIST={};
+
+//player object
+let Player = function(id){
+  let self={
+    x:250,
+    y:250,
+    id:id,
+    number:""+Math.floor(Math.random()*10),
+    pressingRight: false,
+    pressingLeft:false,
+    pressingUp:false,
+    pressingDown:false,
+    maxSpeed:10
+  }
+
+  self.updatePosition = function(){
+    if(self.pressingRight)
+      self.x += self.maxSpeed;
+    if(self.pressingLeft)
+      self.x -= self.maxSpeed;
+    if(self.pressingUp)
+      self.y -= self.maxSpeed;
+    if(self.pressingDown)
+      self.y += self.maxSpeed;
+  }
+  return self;
+}
 
 //load socket.io into the server
 let io = require('socket.io')(serv,{});
-
-//create an array of sockets (clients)
-let SOCKET_LIST = {};
 
 //if a client connects
 io.sockets.on('connection', function(socket){
@@ -23,30 +52,31 @@ io.sockets.on('connection', function(socket){
 
   //give a random id to client
   socket.id = Math.random();
-  //give a position in space (creates object propreties)
-  socket.x = 0;
-  socket.y = 0;
-  //give a random integer (canvas needs string form)
-  socket.number = ""+Math.floor(Math.random()*10);
   //put socket into socket array
   SOCKET_LIST[socket.id]=socket;
 
-
-  //listen to client message
-  socket.on('clientMsg', function(data){
-    console.log(data.reason);
-  });
-
-  //send server message to client
-  socket.emit('serverMsg',{
-    msg:'hi i am a server'
-  });
+  //create player
+  let player = Player(socket.id);
+  PLAYER_LIST[socket.id]=player;
 
   //listen when socket disconnects (doesn't need code in html)
   //delete the client from socket array
   socket.on('disconnect', function(){
     delete SOCKET_LIST[socket.id];
-  })
+    delete PLAYER_LIST[socket.id];
+  });
+
+  socket.on('keyPress', function(data){
+    if(data.inputId==='left'){
+      player.pressingLeft = data.state;
+    } else if(data.inputId==='right'){
+      player.pressingRight = data.state;
+    } else if(data.inputId==='up'){
+      player.pressingUp = data.state;
+    } else if(data.inputId==='down'){
+      player.pressingDown = data.state;
+    }
+  });
 
 });
 
@@ -54,19 +84,18 @@ io.sockets.on('connection', function(socket){
 setInterval(function(){
   let pack = []; //array of parameters of each client
 
-  //update parameters of each client and push to pack
-  for(let i in SOCKET_LIST){
-    let socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+  //update parameters of each player and push to pack
+  for(let i in PLAYER_LIST){
+    let player = PLAYER_LIST[i];
+    player.updatePosition();
     pack.push({
-      x: socket.x,
-      y: socket.y,
-      number: socket.number
+      x: player.x,
+      y: player.y,
+      number: player.number
     });
   }
 
-  //send the pack to client
+  //send the pack to client THROUGH THE SOCKET
   for(let i in SOCKET_LIST){
     let socket = SOCKET_LIST[i];
     socket.emit('newPosition',pack);
