baddef3073bc35009efc5a2fcb932dd897ed91d1
Dana Ryashy <danriachi@hotmail.com>
2019-11-27 18:36:02 -0500

    OOP format, added bullets

--- multiplayer_test/app.js
+++ multiplayer_test/app.js
@@ -12,36 +12,150 @@ serv.listen(2000);//using port 2000
 console.log('server started');
 //************************************************************//
 
-//create an array of sockets (clients) & players
-let SOCKET_LIST = {};
-let PLAYER_LIST={};
+////////////ENTITY CLASS////////////
+let Entity = function(){
+    let self = {
+        x:250,
+        y:250,
+        spdX:0,
+        spdY:0,
+        id:"",
+    }
 
-//player object
+    self.update = function(){
+        self.updatePosition();
+    }
+
+    self.updatePosition = function(){
+        self.x += self.spdX;
+        self.y += self.spdY;
+    }
+    return self;
+}
+
+////////PLAYER CLASS////////
 let Player = function(id){
-  let self={
-    x:250,
-    y:250,
-    id:id,
-    number:""+Math.floor(Math.random()*10),
-    pressingRight: false,
-    pressingLeft:false,
-    pressingUp:false,
-    pressingDown:false,
-    maxSpeed:10
+  let self = Entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.maxSpd = 10;
+
+  //rewrite update of player to include updateSpd
+  let super_update = self.update;
+  self.update = function(){
+      self.updateSpd();
+      super_update();
   }
 
-  self.updatePosition = function(){
-    if(self.pressingRight)
-      self.x += self.maxSpeed;
-    if(self.pressingLeft)
-      self.x -= self.maxSpeed;
-    if(self.pressingUp)
-      self.y -= self.maxSpeed;
-    if(self.pressingDown)
-      self.y += self.maxSpeed;
+  self.updateSpd = function(){
+      if(self.pressingRight)
+          self.spdX = self.maxSpd;
+      else if(self.pressingLeft)
+          self.spdX = -self.maxSpd;
+      else
+          self.spdX = 0;
+
+      if(self.pressingUp)
+          self.spdY = -self.maxSpd;
+      else if(self.pressingDown)
+          self.spdY = self.maxSpd;
+      else
+          self.spdY = 0;
   }
+  //add the player to a list of players
+  Player.list[id] = self;
   return self;
 }
+
+////player functions////
+Player.list = {}; //no longer a global variable
+
+Player.onConnect = function(socket){
+    let player = Player(socket.id);
+    socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+    });
+}
+
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+}
+
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        });
+    }
+    return pack;
+}
+
+//////END OF PLAYER CLASS////////
+
+/////BULLET CLASS////////////
+var Bullet = function(angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+
+    self.timer = 0;
+    self.toRemove = false;
+
+    //rewrite update
+    var super_update = self.update;
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+
+    //add to list
+    Bullet.list[self.id] = self;
+    return self;
+}
+
+Bullet.list = {};
+
+Bullet.update = function(){
+    if(Math.random() < 0.1){
+        Bullet(Math.random()*360);
+    }
+
+    var pack = [];
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });
+    }
+    return pack;
+}
+///////END OF BULLET CLASS//////
+
+//////SOCKET MANIPULATION///////
+//for eval() debugging
+var DEBUG = true;
+//create an array of sockets (clients) & players
+let SOCKET_LIST = {};
 
 //load socket.io into the server
 let io = require('socket.io')(serv,{});
@@ -55,44 +169,32 @@ io.sockets.on('connection', function(socket){
   //put socket into socket array
   SOCKET_LIST[socket.id]=socket;
 
-  //create player
-  let player = Player(socket.id);
-  PLAYER_LIST[socket.id]=player;
+  //handle player connection & give keypress listener
+  Player.onConnect(socket);
 
-  //listen when socket disconnects (doesn't need code in html)
-  //delete the client from socket array
+  //listen when socket disconnects
   socket.on('disconnect', function(){
+      //delete the client from socket array
     delete SOCKET_LIST[socket.id];
-    delete PLAYER_LIST[socket.id];
+    //delete player from player.list
+    Player.onDisconnect(socket);
   });
 
-  socket.on('keyPress', function(data){
-    if(data.inputId==='left'){
-      player.pressingLeft = data.state;
-    } else if(data.inputId==='right'){
-      player.pressingRight = data.state;
-    } else if(data.inputId==='up'){
-      player.pressingUp = data.state;
-    } else if(data.inputId==='down'){
-      player.pressingDown = data.state;
-    }
+  //when eval message received from chatbox, send answer
+  socket.on('evalServer',function(data){
+    if(!DEBUG) //if debugging is not allowed
+        return;
+    var res = eval(data);
+    socket.emit('evalAnswer',res);
   });
 
 });
 
 //update and send parameters of all connected sockets to client (loop)
 setInterval(function(){
-  let pack = []; //array of parameters of each client
-
-  //update parameters of each player and push to pack
-  for(let i in PLAYER_LIST){
-    let player = PLAYER_LIST[i];
-    player.updatePosition();
-    pack.push({
-      x: player.x,
-      y: player.y,
-      number: player.number
-    });
+  let pack = {
+    player:Player.update(),
+    bullet:Bullet.update()
   }
 
   //send the pack to client THROUGH THE SOCKET
